6252d661fd0bcfe5c389a43f4b150a57b14b5c68
Robyn Kirkman <robyn@robynkirkman.us>
2024-11-17 19:28:15 -0500

    edit graph test

--- test/src/alg/edit_graph_test.cpp
+++ test/src/alg/edit_graph_test.cpp
@@ -3,3 +3,33 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+using namespace badiff;
+
+class EditGraphTest : public testing::Test {
+public:
+	virtual ~EditGraphTest() = default;
+
+
+};
+
+TEST_F(EditGraphTest, TestIdentity) {
+	alg::EditGraph graph;
+	std::string hello = "hello";
+
+	ByteArray original(new Byte[hello.size()]);
+	std::copy(hello.begin(), hello.end(), original.get());
+
+	ByteArray target(new Byte[hello.size()]);
+	std::copy(hello.begin(), hello.end(), target.get());
+
+	graph.Compute(original.get(), hello.size(), target.get(), hello.size());
+
+	auto op_queue = graph.MakeOpQueue();
+
+	ASSERT_FALSE(op_queue->IsEmpty());
+	Op op = op_queue->PopFront();
+	ASSERT_EQ(op.GetType(), Op::NEXT);
+	ASSERT_EQ(op.GetLength(), 5);
+	ASSERT_TRUE(op_queue->IsEmpty());
+
+}
