6fc67ffef595685629a171d1289a83176d84d3ef
Robyn Kirkman <robyn@robynkirkman.us>
2024-11-18 06:11:49 -0500

    more testing

--- test/src/alg/edit_graph_test.cpp
+++ test/src/alg/edit_graph_test.cpp
@@ -45,3 +45,21 @@ TEST_F(EditGraphTest, TestHelloWorld) {
 
 	ASSERT_EQ(q::OpQueue::SummarizeConsuming(*op_queue), std::string("+3-2>1+1-2"));
 }
+
+TEST_F(EditGraphTest, TestHelloHellish) {
+	alg::EditGraph graph;
+	std::string hello = "hello world";
+	std::string world = "hellish cruel world";
+
+	ByteArray original(new Byte[hello.size()]);
+	std::copy(hello.begin(), hello.end(), original.get());
+
+	ByteArray target(new Byte[world.size()]);
+	std::copy(world.begin(), world.end(), target.get());
+
+	graph.Compute(original.get(), hello.size(), target.get(), world.size());
+
+	auto op_queue = graph.MakeOpQueue();
+
+	ASSERT_EQ(q::OpQueue::SummarizeConsuming(*op_queue), std::string(">2+1>1+8>1-1>6"));
+}
