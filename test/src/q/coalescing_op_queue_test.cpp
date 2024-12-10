6941798dc369ebb8759994e691e36482b340c4e8
Robyn Kirkman <robyn@robynkirkman.us>
2024-12-09 20:50:00 -0500

    more testing

--- test/src/q/coalescing_op_queue_test.cpp
+++ test/src/q/coalescing_op_queue_test.cpp
@@ -34,3 +34,38 @@ TEST_F(CoalescingOpQueueTest, DDD) {
   ASSERT_EQ(op.GetLength(), 3);
   ASSERT_TRUE(op_queue->IsEmpty());
 }
+
+TEST_F(CoalescingOpQueueTest, DID) {
+  std::unique_ptr<q::OpQueue> op_queue(new q::OpQueue);
+  op_queue->Push(Op(Op::DELETE, 1));
+  op_queue->Push(Op(Op::INSERT, 1));
+  op_queue->Push(Op(Op::DELETE, 1));
+  op_queue.reset(new q::CoalescingOpQueue(std::move(op_queue)));
+  ASSERT_FALSE(op_queue->IsEmpty());
+  Op op;
+  op = std::move(*op_queue->Pop());
+  ASSERT_EQ(op.GetType(), Op::DELETE);
+  ASSERT_EQ(op.GetLength(), 2);
+  op = std::move(*op_queue->Pop());
+  ASSERT_EQ(op.GetType(), Op::INSERT);
+  ASSERT_EQ(op.GetLength(), 1);
+  ASSERT_TRUE(op_queue->IsEmpty());
+}
+
+TEST_F(CoalescingOpQueueTest, DIID) {
+  std::unique_ptr<q::OpQueue> op_queue(new q::OpQueue);
+  op_queue->Push(Op(Op::DELETE, 1));
+  op_queue->Push(Op(Op::INSERT, 1));
+  op_queue->Push(Op(Op::INSERT, 1));
+  op_queue->Push(Op(Op::DELETE, 1));
+  op_queue.reset(new q::CoalescingOpQueue(std::move(op_queue)));
+  ASSERT_FALSE(op_queue->IsEmpty());
+  Op op;
+  op = std::move(*op_queue->Pop());
+  ASSERT_EQ(op.GetType(), Op::DELETE);
+  ASSERT_EQ(op.GetLength(), 2);
+  op = std::move(*op_queue->Pop());
+  ASSERT_EQ(op.GetType(), Op::INSERT);
+  ASSERT_EQ(op.GetLength(), 2);
+  ASSERT_TRUE(op_queue->IsEmpty());
+}
