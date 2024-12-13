470e067332ac26719bac5c396402cc6b731d6b6c
Robyn Kirkman <robyn@robynkirkman.us>
2024-12-13 06:11:20 -0500

    formatting

--- include/badiff/op.hpp
+++ include/badiff/op.hpp
@@ -31,7 +31,7 @@ public:
   Op(Type, Length = 1, Value = nullptr);
   Op(Type, const std::string &);
 
-  Op(const badiff::Op &other);
+  Op(const &other);
   Op(Op &&) = default;
   Op &operator=(const Op &) = delete;
   Op &operator=(Op &&) = default;
