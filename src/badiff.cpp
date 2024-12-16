be12fd746a7d6d208bfbe41ce2619103207fba42
Robyn Kirkman <robyn@robynkirkman.us>
2024-12-16 09:46:24 -0500

    chunk the memory diff

--- src/badiff.cpp
+++ src/badiff.cpp
@@ -35,6 +35,9 @@ std::unique_ptr<q::OpQueue> Wrap(std::unique_ptr<q::OpQueue> op_queue) {
       new q::GraphOpQueue(std::move(op_queue),
                           std::unique_ptr<alg::Graph>(new alg::InertialGraph)));
 
+  op_queue.reset(new q::CoalescingOpQueue(std::move(op_queue)));
+  op_queue.reset(new q::CompactingOpQueue(std::move(op_queue)));
+
   op_queue.reset(new q::MinimizeOpQueue(std::move(op_queue)));
 
   return op_queue;
@@ -45,6 +48,8 @@ std::unique_ptr<Diff> Diff::Make(const char *original, int original_size,
                                  const char *target, int target_size) {
   std::unique_ptr<q::OpQueue> op_queue(
       new q::ReplaceOpQueue(original, original_size, target, target_size));
+
+  op_queue.reset(new q::ChunkingOpQueue(std::move(op_queue), 8192));
 
   op_queue = Wrap(std::move(op_queue));
 
