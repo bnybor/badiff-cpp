a8d971851edb0abc7459fd3366b090a8fa7f4a75
Robyn Kirkman <robyn@robynkirkman.us>
2025-01-20 06:43:35 -0500

    Remove test that relies on incidental behavior.
    This test was misleading; patching a non-original chunk of
    memory is not predictable.

--- test/src/fn_patch_test.cpp
+++ test/src/fn_patch_test.cpp
@@ -59,37 +59,3 @@ TEST_F(FnPatchTest, PatchStruct) {
   ASSERT_EQ(d4.b, d2.b);
   ASSERT_EQ(d4.c, d2.c);
 }
-
-TEST_F(FnPatchTest, PatchNonOriginalStruct) {
-  // This test is someone misleading, operating on integers.
-  // It should be rewritten to operate on character arrays or something.
-  // Treating structs as char* means the atomic unit of diffing
-  // is a byte, not an integer, so diffs may be on an intra-integer level.
-
-  // It is also worth pointing out that badiff is optimized for the size of the
-  // serialized delta, which will always be correct but may not be what you
-  // expect.
-
-  struct Data {
-    int a;
-    int b;
-    int c;
-  };
-
-  auto d1 = Data{.a = 1, .b = 2, .c = 3};
-  auto d2 = Data{.a = 1, .b = 4, .c = 3};
-  // The difference between d1 and d2 is that b was changed to 4.
-  auto delta = badiff::Delta::Make( //
-      (char *)&d1, sizeof(d1),      //
-      (char *)&d2, sizeof(d2));
-
-  auto d3 = Data{.a = 5, .b = 6, .c = 7};
-  auto d4 = Data{.a = 0, .b = 0, .c = 0};
-  // Apply that difference to d3, storing in d4.
-  delta->Apply((char *)&d3, (char *)&d4);
-
-  // d4 should only differ from d3 in that b was changed to 4.
-  ASSERT_EQ(d4.a, 5);
-  ASSERT_EQ(d4.b, 4);
-  ASSERT_EQ(d4.c, 7);
-}
