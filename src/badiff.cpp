ddddd8b7eed5e0ed0ed30ffb53ccb83a26ad421c
Robyn Kirkman <robyn@robynkirkman.us>
2025-02-01 10:54:56 -0500

    Remove code duplication by making a parameterized meet-in-the-middle.

--- src/badiff.cpp
+++ src/badiff.cpp
@@ -41,6 +41,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <badiff/q/stream_replace_op_queue.hpp>
 
 #include <fstream>
+#include <functional>
 #include <istream>
 #include <map>
 #include <ostream>
@@ -93,15 +94,23 @@ std::unique_ptr<q::OpQueue> ComputeDiff(std::unique_ptr<q::OpQueue> op_queue) {
 
   return op_queue;
 }
-} // namespace
 
-std::unique_ptr<Delta> Delta::Make(const char *original, int original_len,
-                                   const char *target, int target_len) {
+struct MakeParameters {
+  std::unique_ptr<q::OpQueue> forward_op_queue_;
+  std::unique_ptr<q::OpQueue> reverse_op_queue_;
+  std::function<char(int)> get_original_;
+  std::function<char(int)> get_target_;
+  int original_len_;
+  int target_len_;
+};
+
+std::unique_ptr<Delta> MakeParameterized(MakeParameters parameters) {
   // OpQueues for the byte arrays, in order and in reverse order.
-  std::unique_ptr<q::OpQueue> op_queue(
-      new q::ReplaceOpQueue(original, original_len, target, target_len));
+  std::unique_ptr<q::OpQueue> op_queue(std::move(parameters.forward_op_queue_));
   std::unique_ptr<q::OpQueue> rop_queue(
-      new q::RReplaceOpQueue(original, original_len, target, target_len));
+      std::move(parameters.reverse_op_queue_));
+  int original_len = parameters.original_len_;
+  int target_len = parameters.target_len_;
 
   op_queue = ComputeDiff(std::move(op_queue));   // forward diff
   rop_queue = ComputeDiff(std::move(rop_queue)); // backward diff
@@ -205,14 +214,18 @@ std::unique_ptr<Delta> Delta::Make(const char *original, int original_len,
   if (original_pos < original_rpos) {
     int len = original_rpos - original_pos;
     std::unique_ptr<char[]> value(new char[len]);
-    std::copy(original + original_pos, original + original_rpos, value.get());
+    for (int pos = original_pos; pos < original_rpos; ++pos) {
+      value[pos - original_pos] = parameters.get_original_(pos);
+    }
     middle->Push(Op(Op::DELETE, len, std::move(value)));
   }
   // Insert the target middle region.
   if (target_pos < target_rpos) {
     int len = target_rpos - target_pos;
     std::unique_ptr<char[]> value(new char[len]);
-    std::copy(target + target_pos, target + target_rpos, value.get());
+    for (int pos = target_pos; pos < target_rpos; ++pos) {
+      value[pos - target_pos] = parameters.get_target_(pos);
+    }
     middle->Push(Op(Op::INSERT, len, std::move(value)));
   }
 
@@ -254,144 +267,49 @@ std::unique_ptr<Delta> Delta::Make(const char *original, int original_len,
   return diff;
 }
 
+} // namespace
+
+std::unique_ptr<Delta> Delta::Make(const char *original, int original_len,
+                                   const char *target, int target_len) {
+  // OpQueues for the byte arrays, in order and in reverse order.
+  std::unique_ptr<q::OpQueue> op_queue(
+      new q::ReplaceOpQueue(original, original_len, target, target_len));
+  std::unique_ptr<q::OpQueue> rop_queue(
+      new q::RReplaceOpQueue(original, original_len, target, target_len));
+
+  MakeParameters parameters;
+  parameters.forward_op_queue_ = std::move(op_queue);
+  parameters.reverse_op_queue_ = std::move(rop_queue);
+  parameters.original_len_ = original_len;
+  parameters.target_len_ = target_len;
+  parameters.get_original_ = [&original](int pos) { return original[pos]; };
+  parameters.get_target_ = [&target](int pos) { return target[pos]; };
+
+  return MakeParameterized(std::move(parameters));
+}
+
 std::unique_ptr<Delta> Delta::Make(std::istream &original, int original_len,
                                    std::istream &target, int target_len) {
-  /*
-   * See Delta::Make(const char*, int, const char*, intO).
-   *
-   * The only difference is how the middle buffer is taken from the original and
-   * target (array index vs istream seekg)
-   */
   std::unique_ptr<q::OpQueue> op_queue(
       new q::StreamReplaceOpQueue(original, original_len, target, target_len));
   std::unique_ptr<q::OpQueue> rop_queue(
       new q::RStreamReplaceOpQueue(original, original_len, target, target_len));
 
-  op_queue = ComputeDiff(std::move(op_queue));
-  rop_queue = ComputeDiff(std::move(rop_queue));
-  rop_queue.reset(new q::ROpQueue(std::move(rop_queue)));
+  MakeParameters parameters;
+  parameters.forward_op_queue_ = std::move(op_queue);
+  parameters.reverse_op_queue_ = std::move(rop_queue);
+  parameters.original_len_ = original_len;
+  parameters.target_len_ = target_len;
+  parameters.get_original_ = [&original](int pos) {
+    original.seekg(pos);
+    return original.get();
+  };
+  parameters.get_target_ = [&target](int pos) {
+    target.seekg(pos);
+    return target.get();
+  };
 
-  // Merge the original and reverse diffs
-  int original_pos = 0, target_pos = 0;
-  int original_rpos = original_len, target_rpos = target_len;
-
-  std::vector<std::unique_ptr<Op>> diff_ops, rdiff_ops;
-
-  std::unique_ptr<Op> next_op, rnext_op;
-  while (true) {
-    if (!next_op && !op_queue->IsEmpty()) {
-      next_op = op_queue->Pop();
-    }
-    if (!rnext_op && !rop_queue->IsEmpty()) {
-      rnext_op = rop_queue->Pop();
-    }
-    if (!next_op && !rnext_op)
-      break;
-    int next_original_pos = original_pos, next_original_rpos = original_rpos;
-    int next_target_pos = target_pos, next_target_rpos = target_rpos;
-
-    if (next_op) {
-      if (next_op->GetType() != Op::INSERT)
-        next_original_pos += next_op->GetLength();
-      if (next_op->GetType() != Op::DELETE)
-        next_target_pos += next_op->GetLength();
-    }
-    if (rnext_op) {
-      if (rnext_op->GetType() != Op::INSERT)
-        next_original_rpos -= rnext_op->GetLength();
-      if (rnext_op->GetType() != Op::DELETE)
-        next_target_rpos -= rnext_op->GetLength();
-    }
-
-    if (next_original_pos >= next_original_rpos)
-      break;
-    if (next_target_pos >= next_target_rpos)
-      break;
-
-    if (next_op && !rnext_op) {
-      diff_ops.insert(diff_ops.end(), std::move(next_op));
-      original_pos = next_original_pos;
-      target_pos = next_target_pos;
-    } else if (!next_op && rnext_op) {
-      rdiff_ops.insert(rdiff_ops.begin(), std::move(rnext_op));
-      original_rpos = next_original_rpos;
-      target_rpos = next_target_rpos;
-    } else if (next_op && rnext_op) {
-      int next_len = 0;
-      if (next_op->GetType() == Op::INSERT)
-        next_len = next_op->GetLength();
-      if (next_op->GetType() == Op::NEXT)
-        next_len = -next_op->GetLength();
-      int rnext_len = 0;
-      if (rnext_op->GetType() == Op::INSERT)
-        rnext_len = rnext_op->GetLength();
-      if (rnext_op->GetType() == Op::NEXT)
-        rnext_len = -rnext_op->GetLength();
-      if (next_len <= rnext_len) {
-        diff_ops.insert(diff_ops.end(), std::move(next_op));
-        original_pos = next_original_pos;
-        target_pos = next_target_pos;
-      } else {
-        rdiff_ops.insert(rdiff_ops.begin(), std::move(rnext_op));
-        original_rpos = next_original_rpos;
-        target_rpos = next_target_rpos;
-      }
-    }
-  }
-
-  std::unique_ptr<q::OpQueue> middle(new q::OpQueue);
-
-  if (original_pos < original_rpos) {
-    int len = original_rpos - original_pos;
-    std::unique_ptr<char[]> value(new char[len]);
-    original.seekg(original_pos);
-    for (int i = 0; i < len; ++i) {
-      value[i] = original.get();
-    }
-    middle->Push(Op(Op::DELETE, len, std::move(value)));
-  }
-  if (target_pos < target_rpos) {
-    int len = target_rpos - target_pos;
-    std::unique_ptr<char[]> value(new char[len]);
-    target.seekg(target_pos);
-    for (int i = 0; i < len; ++i) {
-      value[i] = target.get();
-    }
-    middle->Push(Op(Op::INSERT, len, std::move(value)));
-  }
-
-  middle.reset(new q::ChunkingOpQueue(std::move(middle)));
-  middle.reset(new q::GraphOpQueue(
-      std::move(middle), std::unique_ptr<alg::Graph>(new alg::InertialGraph),
-      q::GraphOpQueue::ANY));
-  middle.reset(new q::MinimizeOpQueue(std::move(middle)));
-
-  op_queue.reset(new q::OpQueue);
-  for (auto &op : diff_ops) {
-    op_queue->Push(*std::move(op));
-  }
-
-  while (!middle->IsEmpty()) {
-    op_queue->Push(*middle->Pop());
-  }
-
-  for (auto &op : rdiff_ops) {
-    op_queue->Push(*std::move(op));
-  }
-
-  std::ostringstream ss;
-  op_queue->Serialize(ss);
-  auto str = ss.str();
-
-  std::unique_ptr<Delta> diff(new Delta);
-  diff->original_len_ = original_len;
-  diff->target_len_ = target_len;
-  diff->delta_len_ = str.size();
-  diff->delta_.reset(new char[str.size()]);
-
-  std::copy(str.c_str(), str.c_str() + str.size(), diff->delta_.get());
-
-  return diff;
+  return MakeParameterized(std::move(parameters));
 }
 
 std::unique_ptr<Delta> Delta::Make(std::string original_file,
